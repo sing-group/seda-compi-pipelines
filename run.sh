473a6a0891ada20d30b8ac9b0ea3292dbdf34453
Hugo López-Fernández <hlfernandez@uvigo.es>
2023-05-15 17:44:26 +0200

    Adds docker run params to the docker run command to allow using the SEDA_JAVA_MEMORY environment variable

--- run.sh
+++ run.sh
@@ -19,9 +19,16 @@ if [ -f "${WORKING_DIR}/compi.params" ]; then
     PIPELINE_PARAMS_FILE="--params ${WORKING_DIR}/compi.params"
 fi
 
+DOCKER_ENV_PARAMS=""
+if [ ! -z "${SEDA_JAVA_MEMORY}" ]; then
+    DOCKER_ENV_PARAMS="-e SEDA_JAVA_MEMORY=${SEDA_JAVA_MEMORY}"
+    echo "${DOCKER_ENV_PARAMS}"
+fi
+
 docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v /tmp:/tmp \
     -v "${WORKING_DIR}:${WORKING_DIR}" \
     -w "${WORKING_DIR}" \
+    ${DOCKER_ENV_PARAMS} \
     test/seda-cli-pipeline \
         /compi run -p /pipeline.xml -o -r /pipeline-runner.xml ${COMPI_PARAMS} ${PIPELINE_PARAMS_FILE} -- \
             --workingDirectory ${WORKING_DIR}
