e74bc25b584341cae71b16e6b6170a9f711cd408
Hugo López-Fernández <hlfernandez@uvigo.es>
2023-05-18 20:34:19 +0200

    Adds log messages to the main run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,12 @@
 WORKING_DIR=${1}
 COMPI_PARAMS=${2:-""}
 
+function info() {
+    tput setaf 2
+	echo -e "[INFO] [run.sh] ${1}"
+	tput sgr0
+}
+
 function show_error() {
 	tput setaf 1
 	echo -e "${1}"
@@ -14,15 +20,18 @@ if [ $# -ne 1 ] && [ $# -ne 2 ]; then
 	exit 1
 fi
 
+info "Running SEDA-Compi pipeline at ${WORKING_DIR}"
+
 PIPELINE_PARAMS_FILE=""
 if [ -f "${WORKING_DIR}/compi.params" ]; then
     PIPELINE_PARAMS_FILE="--params ${WORKING_DIR}/compi.params"
+    info "Pipeline parameters found at ${WORKING_DIR}/compi.params"
 fi
 
 DOCKER_ENV_PARAMS=""
 if [ ! -z "${SEDA_JAVA_MEMORY}" ]; then
     DOCKER_ENV_PARAMS="-e SEDA_JAVA_MEMORY=${SEDA_JAVA_MEMORY}"
-    echo "${DOCKER_ENV_PARAMS}"
+    info "Docker environment parameters: ${DOCKER_ENV_PARAMS}"
 fi
 
 docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v /tmp:/tmp \
